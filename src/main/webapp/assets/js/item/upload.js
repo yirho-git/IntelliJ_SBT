c8b3acbfc7e76c10fa57a85c0fdb1899b634949c
Yirho <yirho0528@gmail.com>
2026-03-29 03:03:52 +0900

    파일업로드 기능구현

---
+++ src/main/webapp/assets/js/item/upload.js
@@ -0,0 +1,26 @@
+
+ document.addEventListener("DOMContentLoaded", () => {
+
+     const inputFile = document.querySelector(`input[name="inputFile"]`);
+
+     inputFile.addEventListener("change", async (e) => {
+         let files = e.target.files;
+         let file = files[0];
+
+         let formData = new FormData();
+         formData.append("file", file);
+
+         try {
+             const resp = await fetch("/item/uploadFile",{
+                 method : "post",
+                 body : formData
+             });
+
+             const data = await resp.text();
+             console.log("data = resp.text() = > ", data);
+         }catch (error) {
+             console.log("error = " + error);
+         }
+     });
+
+})
